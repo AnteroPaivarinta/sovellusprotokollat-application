c9472946b93c0f978acaa220ead67849dda7cb17
AnteroPaivarinta <antsapaivarinta@gmail.com>
2025-01-22 06:51:34 +0200

    remove console.log

--- smtp-server/functions/readMails.js
+++ smtp-server/functions/readMails.js
@@ -3,11 +3,8 @@ import fs from 'fs';
 export const readMails = (user, socket) => {
     console.log("USER", user, "SOCKET", socket);
   fs.readdir("./Maildir/inbox", (err, files) => {
-    console.log("HALO12342343");
       files.forEach(file => {
-        console.log("HALO13");
           if(file.includes(user)) {
-            console.log("HALO1");
               fs.readFile(`./Maildir/inbox/${file}`, 'utf8', (err, data) => {
                   if (err) {
                       console.error('Virhe tiedoston lukemisessa:', err);
